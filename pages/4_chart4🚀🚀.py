ec927091f3976dbf25663ff5c2d920bd6eb91839
633230007 <144574821+633230007@users.noreply.github.com>
2023-10-08 10:58:40 +0700

    .commit

--- pages/4_chart4🚀🚀.py
+++ pages/4_chart4🚀🚀.py
@@ -4,11 +4,11 @@ import requests
 import streamlit as st
 import pandas as pd
 
-st.header("Show Data Index Price")
-df=pd.read_csv("./data/stock_index_price.csv")
+st.header("Show Data breast_cancer")
+df=pd.read_csv("./data/breast_cancer.csv")
 st.write(df.head(10))
 
 st.header("Show Chart")
 st.line_chart(
-   df, x="stock_index_price", y=["interest_rate", "unemployment_rate"], color=["#FF0000", "#0000FF"]  # Optional
+   df, x="breast_cancer", y=["interest_rate", "unemployment_rate"], color=["#FF0000", "#0000FF"]  # Optional
 )
