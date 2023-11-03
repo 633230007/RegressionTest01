45ff1690b6da65ff9ae1a9dc2922491d6cc9b123
633230007 <144574821+633230007@users.noreply.github.com>
2023-11-04 03:50:56 +0700

    Update 4_chart4🚀🚀.py

--- pages/4_chart4🚀🚀.py
+++ pages/4_chart4🚀🚀.py
@@ -13,11 +13,11 @@ df=pd.read_csv('./data/Student Stress Factors.csv')
 st.write(df.head(10))
 
 #st.line_chart(df)
-#st.line_chart(df, x="Timestamp", y="Kindly Rate your Sleep Quality", color="Student Stress Factors")
-st.line_chart(df, x="Timestamp", y=["Kindly Rate your Sleep Quality", "Student Stress Factors"], color=["#FF0000", "#0000FF"]  # Optional
+#st.line_chart(df, x="Sleep Quality", y="headaches", color="Student Stress Factors")
+st.line_chart(df, x="Sleep Quality", y=["headaches", "Student Stress Factors"], color=["#FF0000", "#0000FF"]  # Optional
 )
 
-x=df[['Timestamp','Kindly Rate your Sleep Quality']]
+x=df[['Sleep Quality','headaches']]
 y=df['Student Stress Factors']
 pf=PolynomialFeatures(degree=3)
 x_poly=pf.fit_transform(x)
@@ -26,8 +26,8 @@ x_train,x_test,y_train,y_test =train_test_split(x_poly,y,random_state=0)
 
 modelRegress=LinearRegression()
 modelRegress.fit(x_train,y_train)
-x1=st.number_input("กรุณาป้อนข้อมูล Timestamp:")
-x2=st.number_input("กรุณาป้อนข้อมูล Kindly Rate your Sleep Quality:")
+x1=st.number_input("กรุณาป้อนข้อมูล Sleep Quality:")
+x2=st.number_input("กรุณาป้อนข้อมูล headaches:")
 
 if st.button("พยากรณ์ข้อมูล"):
     x_input=[[x1,x2]]
