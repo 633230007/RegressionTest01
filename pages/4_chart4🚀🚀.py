9a16bc9f40de8337a4ab7ad825f0fac009525a8e
633230007 <144574821+633230007@users.noreply.github.com>
2023-11-04 07:42:56 +0700

    Update 4_chart4🚀🚀.py

--- pages/4_chart4🚀🚀.py
+++ pages/4_chart4🚀🚀.py
@@ -9,8 +9,11 @@ from sklearn.metrics import r2_score,mean_squared_error
 st.title("Student Stress Factors")
 st.header("Student Stress Factors")
 
-df=pd.read_csv('./data/Student Stress Factors.csv')
+df=pd.read_csv('./data/Factors.csv')
 st.write(df.head(10))
+
+x = df.iloc[:, :-1]  # ยกเว้นคอลัมน์สุดท้าย
+y = df.iloc[:, -1]   # คอลัมน์สุดท้ายเป็น target
 
 #st.line_chart(df)
 #st.line_chart(df, x="Sleep Quality", y="headaches", color="Student Stress Factors")
@@ -21,16 +24,23 @@ y=df['Student Stress Factors']
 pf=PolynomialFeatures(degree=3)
 x_poly=pf.fit_transform(x)
 
-x_train,x_test,y_train,y_test =train_test_split(x_poly,y,random_state=0)
+x_train, x_test, y_train, y_test = train_test_split(x_poly, y, test_size=0.2, random_state=1)
+sc = StandardScaler()
+x_train = sc.fit_transform(x_train)
+x_test = sc.transform(x_test)
 
-modelRegress=LinearRegression()
-modelRegress.fit(x_train,y_train)
+modelDtree=DecisionTreeClassifier(criterion='gini')
+modelDtree.fit(x_train, y_train)
 x1=st.number_input("กรุณาป้อนข้อมูล Sleep Quality:")
 x2=st.number_input("กรุณาป้อนข้อมูล headaches:")
+x3=st.number_input("กรุณาป้อนข้อมูล academic performance:")
+x4=st.number_input("กรุณาป้อนข้อมูล study load:")
+x5=st.number_input("กรุณาป้อนข้อมูล extracurricular activities:")
+x6=st.number_input("กรุณาป้อนข้อมูล stress levels:")
 
 if st.button("พยากรณ์ข้อมูล"):
-    x_input=[[x1,x2]]
-    y_predict=modelRegress.predict(pf.fit_transform(x_input))
+    x_input=[[x1, x2, x3, x4, x5, x6]]
+    y_predict=modelDtree.predict(pf.fit_transform(x_input))
     st.write(y_predict)
     st.button("ไม่พยากรณ์ข้อมูล")
 else:
